9fe6ad0c8123aae82d200bc81e89f243b256c051
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-06-02 16:40:51 +0200

    feat : scholarship contracts

--- scholarship-dapp/contracts/Scholarship.sol
+++ scholarship-dapp/contracts/Scholarship.sol
@@ -1,2 +1,39 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+
+contract Scholarship {
+    address public admin;
+    mapping(address => bool) public applicants;
+    uint256 public totalDonations;
+
+    event Donated(address indexed donor, uint256 amount);
+    event Applied(address indexed applicant);
+    event FundsReleased(address indexed recipient, uint256 amount);
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function donate() public payable {
+        require(msg.value > 0, "Donation must be greater than 0");
+        totalDonations += msg.value;
+        emit Donated(msg.sender, msg.value);
+    }
+
+    function applyForScholarship() public {
+        require(!applicants[msg.sender], "already applied");
+        applicants[msg.sender] = true;
+        emit Applied(msg.sender);
+    }
+
+    function releaseFunds(address payable recipient, uint256 amount) public {
+        require(msg.sender == admin, "Only admins can release funds");
+        require(address(this).balance >= amount, "Insufficient funds");
+        recipient.transfer(amount);
+        emit FundsReleased(recipient, amount);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
